aadef87e1d8e473ab0233ea03ab93625e744131a
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-01 09:48:41 -0700

    testing2

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ onMounted(async () => {
 
 <template>
   <div class="p-6">
-    <h1 class="text-2xl font-bold mb-4">Daftar Pengguna</h1>
+    <h1 class="text-2xl font-bold mb-4">Testing</h1>
     <ul>
       <li v-for="user in users" :key="user.id" class="mb-2">
         {{ user.name }} ({{ user.email }})
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/submission_specialtask_web_anargyaits_sukma/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/submission_specialtask_web_anargyaits_sukma/', // sesuaikan dengan nama repo kamu!
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
