9505aa99f709e5fe099a90f8a4e8737861a989c7
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-01 07:47:11 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import api from './services/api.js'
+import { ref, onMounted } from 'vue'
+
+const users = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await api.get('/users')
+    users.value = response.data
+    console.log(response.data)
+  } catch (error) {
+    console.error('Error fetching users:', error)
+  }
+})
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Daftar Pengguna</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id" class="mb-2">
+        {{ user.name }} ({{ user.email }})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/submission_specialtask_web_anargyaits_sukma/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
